250036779198814500a6d028a3f8b53db17acfdc
BrettBass <BrettBass@github.com>
2022-08-07 11:35:29 -0700

    help format version 1
    Signed-off-by: BrettBass <BrettBass@github.com>

--- util/CustomHelpFormatter.cs
+++ util/CustomHelpFormatter.cs
@@ -8,10 +8,16 @@ namespace discordBot.util;
 public class CustomHelpFormatter : DefaultHelpFormatter
 {
     protected DiscordEmbedBuilder _embed;
+    // protected StringBuilder _strBuilder;
 
     public CustomHelpFormatter(CommandContext ctx) : base(ctx)
     {
         _embed = new DiscordEmbedBuilder();
+        _embed.Color = new Optional<DiscordColor>(DiscordColor.SapGreen);
+        _embed.WithThumbnail(
+            "https://akns-images.eonline.com/eol_images/Entire_Site/2019911/rs_600x600-191011143636-600x600-nancy-gj-10-11-19.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top");
+
+        // _strBuilder = new StringBuilder();
 
         // Help formatters do support dependency injection.
         // Any required services can be specified by declaring constructor parameters. 
@@ -21,28 +27,34 @@ public class CustomHelpFormatter : DefaultHelpFormatter
 
     public override BaseHelpFormatter WithCommand(Command command)
     {
-        _embed.AddField(command.Name, command.Description);            
+        _embed.Title = "**" + command.Name + "**";
+        _embed.WithThumbnail(
+            "https://arc-anglerfish-arc2-prod-bostonglobe.s3.amazonaws.com/public/6VTZYSW4PAI6PA6TAHWDXIMC6E.jpg");
+        _embed.AddField("Aliases", String.Join(" ", command.Aliases), true);
+        _embed.AddField("Description", command.Description);
 
         return this;
     }
 
     public override BaseHelpFormatter WithSubcommands(IEnumerable<Command> cmds)
     {
+        _embed.Title = "**Help**";
+        String commands = "Commands";
+        String output = "";
         foreach (var cmd in cmds)
         {
-            _embed.AddField(cmd.Name, cmd.Description);
-        }
+            if (cmd.IsHidden || cmd.Name is "help") continue;
 
+            output += "`" + cmd.Name + "`\n";
+        }
+    
+        if (output is not "") _embed.AddField(commands, output);
         return this;
     }
-    //TODO Sort commands by Module
-    public IEnumerable<Command> Sort(IEnumerable<Command> cmds)
-    {
 
-        return cmds;
-    }
     public override CommandHelpMessage Build()
     {
         return new CommandHelpMessage(embed: _embed);
+        // return new CommandHelpMessage(content: _strBuilder.ToString());
     }
 }
