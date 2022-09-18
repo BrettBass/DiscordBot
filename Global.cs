dd3c70e5e61df2aeb96fd9305185a26b625b5c52
Brett Bass <bbass@ucsd.edu>
2022-09-17 17:11:01 -0700

    Removed files no longer in use

--- Global.cs
+++
@@ -1,6 +0,0 @@
-namespace DiscordBot;
-static class Global
-{
-    public static readonly string ConfigYml = "/home/brett/projects/DiscordBot/storage/config.yml";
-    public static readonly string JsonDatabase = "/home/brett/projects/DiscordBot/storage/DrinkExchange.json";
-}
