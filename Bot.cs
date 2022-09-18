f206fe8fef9a2b7a16e154856136b21d88b41a89
Brett Bass <bbass@ucsd.edu>
2022-09-17 17:09:08 -0700

    New Command modules

--- Bot.cs
+++ Bot.cs
@@ -1,60 +1,58 @@
-using DSharpPlus;
-using DSharpPlus.Interactivity;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.EventArgs;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Yaml;
+using System.Diagnostics;
 using DiscordBot.Modules;
 using discordBot.util;
+using DotNetEnv;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Executors;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
 
 namespace DiscordBot;
 
 public class Bot
 {
-    public  InteractivityExtension Interactivity { get; private set; }
+    public InteractivityExtension Interactivity { get; private set; }
     public DiscordClient Client { get; private set; }
     public CommandsNextExtension Commands { get; private set; }
 
     public async Task RunAsync()
     {
-        var configBuilder = new ConfigurationBuilder()
-            //.SetBasePath(AppContext.BaseDirectory)
-            .AddYamlFile(Global.ConfigYml)
-            .Build();
-
-        var config = new DiscordConfiguration()
+        Env.TraversePath().Load();
+        Debug.Assert(Environment.GetEnvironmentVariable("TOKEN") != null, "Null TOKEN");
+        var config = new DiscordConfiguration
         {
-            Token = configBuilder["tokens:discord"],
-            TokenType = DSharpPlus.TokenType.Bot,
-            AutoReconnect = true,
+            Token = Environment.GetEnvironmentVariable("TOKEN"),
+            TokenType = TokenType.Bot,
+            AutoReconnect = true
         };
         Client = new DiscordClient(config);
         Client.Ready += OnClientReady;
 
         Client.UseInteractivity(new InteractivityConfiguration
         {
-            Timeout = TimeSpan.FromMinutes(2),
+            Timeout = TimeSpan.FromMinutes(2)
         });
 
         // Client.UseSlashCommands(new SlashCommandsConfiguration
         // {
         //     
         // });
-    
-        var commandsConfig = new CommandsNextConfiguration()
-            {
-                StringPrefixes = new[] { configBuilder["prefix"] },
-                EnableDms = true,
-                EnableMentionPrefix = true,
-                DmHelp = true,
-                IgnoreExtraArguments = true,
-                CommandExecutor = new ParallelQueuedCommandExecutor()
-                
-            };
+
+        var commandsConfig = new CommandsNextConfiguration
+        {
+            StringPrefixes = new[] { Environment.GetEnvironmentVariable("PREFIX") }!,
+            EnableDms = true,
+            EnableMentionPrefix = true,
+            DmHelp = true,
+            IgnoreExtraArguments = true,
+            CommandExecutor = new ParallelQueuedCommandExecutor(Int32.Parse(Environment.GetEnvironmentVariable("WORKERS") ?? "1"))
+        };
         Commands = Client.UseCommandsNext(commandsConfig);
+        Commands.RegisterCommands<AdminCommandModule>();
         Commands.RegisterCommands<BankCommandModule>();
+        Commands.RegisterCommands<BarCommandModule>();
         Commands.RegisterCommands<PrefixModule>();
         Commands.RegisterCommands<ResponseModule>();
         Commands.RegisterCommands<DrinkingGameModule>();
@@ -63,6 +61,9 @@ public class Bot
         await Client.ConnectAsync();
         await Task.Delay(-1);
     }
-    private Task OnClientReady(DiscordClient sender, ReadyEventArgs readyEventArgs) { return Task.CompletedTask; }
-    
+
+    private Task OnClientReady(DiscordClient sender, ReadyEventArgs readyEventArgs)
+    {
+        return Task.CompletedTask;
+    }
 }
