a35fe9dcf4be25ff6c306b8af5b2117adc161d6f
Brett Bass <bbass@ucsd.edu>
2022-09-17 17:14:22 -0700

    context for migrations

---
+++ Context/SqliteContext.cs
@@ -0,0 +1,14 @@
+﻿using DiscordBot.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordBot.Context;
+
+public class SqliteContext : DbContext
+{
+    public readonly string ConnectionString = Environment.GetEnvironmentVariable("DISCORDBOTDB_CONNECTIONSTRING") ?? "Data Source=/src/storage/Sqlite.db";
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseSqlite(ConnectionString);
+}
