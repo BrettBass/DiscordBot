fc053e8ecc5c97d8dab3e3beb563f0eac595659b
Brett Bass <bbass@ucsd.edu>
2022-09-17 17:05:32 -0700

    Admin can Init DB

---
+++ Modules/AdminCommandModule.cs
@@ -0,0 +1,28 @@
+﻿using DiscordBot.Context;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordBot.Modules;
+
+[RequireUserPermissions(Permissions.Administrator)]
+public class AdminCommandModule: BaseCommandModule
+{
+    [Command("migratelite")]
+    public async Task MigrateLite(CommandContext ctx)
+    {
+        Console.WriteLine("*** migrating... ***");
+
+        await using var lite = new SqliteContext();
+
+        if (lite.Database.GetPendingMigrationsAsync().Result.Any())
+        {
+            try{await lite.Database.MigrateAsync();}
+            catch(Exception e) { Console.WriteLine(e.InnerException?.Message);}
+            
+        }
+
+        await ctx.Channel.SendMessageAsync("Sqlite Migration complete.");
+    }
+}
