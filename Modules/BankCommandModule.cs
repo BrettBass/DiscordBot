1f2e323f1c27403b59148c8611793d8ac3f2112f
Brett Bass <bbass@ucsd.edu>
2022-09-17 17:07:35 -0700

    Updated to match Bank.cs

--- Modules/BankCommandModule.cs
+++ Modules/BankCommandModule.cs
@@ -15,67 +15,35 @@ public class BankCommandModule : BaseCommandModule
     {
         //get the id of user who sent the command
         var user = ctx.User;
-        var userdata = DrinkExchange.GetUserData(user);
+        var userdata = Bank.GetUserData(user);
 
         var embed = new DiscordEmbedBuilder()
-            .WithTitle(DrinkExchange.Name)
+            .WithTitle(Bank.BankName)
             .WithColor(new DiscordColor(0, 255, 0))
-            .WithThumbnail("https://melmagazine.com/wp-content/uploads/2022/04/Drunken_Monkey_Hypothesis-1024x427.jpg")
+            .WithThumbnail(Bank.BankLogo)
             .AddField("User", user.Username)
-            .AddField("Chips", userdata._chipBalance.ToString(CultureInfo.InvariantCulture), true)
-            .AddField("Drinks owed", userdata._barTab.ToString(CultureInfo.InvariantCulture), true)
-            .AddField("Trustworthiness", userdata._trustworthy.ToString(CultureInfo.CurrentCulture), true);
+            .AddField(Bank.Currency, userdata.Tigers.ToString(CultureInfo.InvariantCulture), true)
+            .AddField("Drinks", userdata.Tab.ToString(CultureInfo.InvariantCulture), true)
+            .AddField("Trustworthiness", userdata.Trust.ToString(CultureInfo.CurrentCulture), true);
 
         await ctx.Channel.SendMessageAsync(embed.Build());
     }
 
     [Command("deposit")]
-    public async Task Deposit(CommandContext ctx, double amount)
+    public async Task Deposit(CommandContext ctx, int amount)
     {
-        DrinkExchange.Deposit(ctx.User, amount);
+        Bank.Deposit(ctx.User, amount);
         await ctx.Channel.SendMessageAsync("Deposited " + amount).ConfigureAwait(false);
     }
 
     //case insensitive command withdraw
     [Command("withdraw")]
-    public async Task Withdraw(CommandContext ctx, double amount)
+    public async Task Withdraw(CommandContext ctx, int amount)
     {
-        if (DrinkExchange.Withdraw(ctx.User, amount))
+        if (Bank.Withdraw(ctx.User, amount))
             await ctx.Channel.SendMessageAsync("Withdrew " + amount).ConfigureAwait(false);
         else
             await ctx.Channel.SendMessageAsync("Invalid amount").ConfigureAwait(false);
     }
-
-    [Command("addDrink")]
-    [Description("addDrink <user> <amount>")]
-    [Aliases("ad")]
-    public async Task AddDrink(CommandContext ctx, DiscordUser user, int amount)
-    {
-        DrinkExchange.AddDrink(user, amount);
-        await ctx.Channel.SendMessageAsync("Added " + amount + " drinks to " + user.Username).ConfigureAwait(false);
-    }
-
-    [Command("drink")]
-    [Description("drink <amount>")]
-    [Aliases("d")]
-    public async Task Drink(CommandContext ctx, int amount = 1)
-    {
-        await ctx.Channel.SendMessageAsync($"{ctx.User.Username}\n{amount} drink/s removed from tab").ConfigureAwait(false);
-        DrinkExchange.TakeDrink(ctx.User, amount);
-    }
-
-    [Command("tab")]
-    [Description("Tab <user?>")]
-    [Aliases("t")]
-    public async Task Tab(CommandContext ctx, DiscordUser? user = null)
-    {
-        if (user == null) user = ctx.User;
-        var embed = new DiscordEmbedBuilder()
-            .WithTitle("Tab")
-            .WithThumbnail(DrinkExchange.Logo)
-            .AddField("User", user.Username)
-            .AddField("Drinks", DrinkExchange.Tab(user).ToString(CultureInfo.InvariantCulture));
-
-        await ctx.Channel.SendMessageAsync(embed.Build()).ConfigureAwait(false);
-    }
+    
 }
