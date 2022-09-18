f4af343779cc2e749d1cf816d6c6eac6da3c124b
Brett Bass <bbass@ucsd.edu>
2022-09-17 17:08:17 -0700

    Move Drink related tasks to new Bar class and module

---
+++ Modules/BarCommandModule.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using discordBot.util;
+
+namespace DiscordBot.Modules;
+
+public class BarCommandModule : BaseCommandModule
+{
+    [Command("addDrink")]
+    [Description("addDrink <user> <amount>")]
+    [Aliases("ad")]
+    public async Task AddDrink(CommandContext ctx, DiscordUser user, int amount)
+    {
+        Bar.AddDrinks(user, amount);
+        await ctx.Channel.SendMessageAsync("Added " + amount + " drinks to " + user.Username).ConfigureAwait(false);
+    }
+
+    [Command("drink")]
+    [Description("drink <amount>")]
+    [Aliases("d")]
+    public async Task Drink(CommandContext ctx, int amount = 1)
+    {
+        await ctx.Channel.SendMessageAsync($"{ctx.User.Username}\n{amount} drink/s removed from tab").ConfigureAwait(false);
+        Bar.TakeDrinks(ctx.User, amount);
+    }
+
+    [Command("tab")]
+    [Description("Tab <user?>")]
+    [Aliases("t")]
+    public async Task Tab(CommandContext ctx, DiscordUser? user = null)
+    {
+        if (user == null) user = ctx.User;
+        var embed = new DiscordEmbedBuilder()
+            .WithTitle("Tab")
+            .WithThumbnail(Bar.Logo)
+            .AddField("User", user.Username)
+            .AddField("Drinks", Bar.Tab(user).ToString(CultureInfo.InvariantCulture));
+
+        await ctx.Channel.SendMessageAsync(embed.Build()).ConfigureAwait(false);
+    }
+}
---
+++ util/Bar.cs
@@ -0,0 +1,32 @@
+﻿using DiscordBot;
+using DSharpPlus.Entities;
+
+namespace discordBot.util;
+
+
+
+public class Bar
+{
+    public static readonly string Logo = Environment.GetEnvironmentVariable("BARLOGO") ??
+                                            "https://melmagazine.com/wp-content/uploads/2022/04/Drunken_Monkey_Hypothesis-1024x427.jpg";
+
+    public static string Column = "Tab";
+    
+    public static void AddDrinks(DiscordUser user, int amount)
+    {
+        if (!DiscordBotDbModifier.Exists(user.Id)) DiscordBotDbModifier.AddEntity(user.Id);
+        DiscordBotDbModifier.Increment(user.Id, Column, amount);
+    }
+
+    public static int Tab(DiscordUser user)
+    {
+        if (!DiscordBotDbModifier.Exists(user.Id)) DiscordBotDbModifier.AddEntity(user.Id);
+        return DiscordBotDbModifier.Pull<int>(user.Id, Column);
+    }
+
+    public static void TakeDrinks(DiscordUser user, int amount)
+    {
+        if (!DiscordBotDbModifier.Exists(user.Id)) DiscordBotDbModifier.AddEntity(user.Id);
+        DiscordBotDbModifier.Decrement(user.Id, Column, amount);
+    }
+}
