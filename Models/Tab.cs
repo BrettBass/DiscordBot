7c1cace1c033ae05da2f3170c2fd356ce1f7d155
Brett Bass <bbass@ucsd.edu>
2022-09-19 16:15:39 -0700

    New Tab.cs to hold drinking data in separate table from banking

---
+++ Models/Tab.cs
@@ -0,0 +1,18 @@
+﻿namespace DiscordBot.Models;
+
+public sealed class Tab : Entity
+{
+    public double Drinks { get; set; }
+    public double Shots { get; set; }
+
+    public Tab(ulong id, int drinks = 0, int shots = 0)
+    {
+        (Id, Drinks, Shots) = (id, drinks, shots);
+    }
+    
+   public override string TableEntityString() { return "Tab(Id, Drinks, Shots)"; }
+   public override string ToEntityString() { return $"{Id}, {Drinks}, {Shots}"; }
+   
+   public override string Table() { return "Tab"; }
+   
+}
